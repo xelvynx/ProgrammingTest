63946eeab3c4d236daf5cae86d157d6e445d5c30
xelvynx <elvynleviste@gmail.com>
2020-07-17 16:00:17 -0700

    Setup Game Space, Added art and scripts for foundation
    Added Background and Trashcan art and setup the scene before I begin adding functionality of plates, cutting boards, players, customers.

---
+++ Tentwork Technical Assessment/Assets/Scripts/Vegetables.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetables : MonoBehaviour
+{
+    //Each vegetable costs a certain amount of time to cut
+}
